ec83b99e6efe848b503b3e0267943a2074f0620a
VintageMocha <zendromida@gmail.com>
2019-05-03 10:03:44 -0700

    Update
    It's not pushing automatically, but that's alright.

--- _js/proj.js
+++ _js/proj.js
@@ -3,5 +3,7 @@ document.getElementById("imageClick").onclick = function() {myFunction()};
 function myFunction() {
   document.getElementById("imageClick").innerHTML = "Congratulations, you found my javascript";}
 
-  // function myFunction() {
-  //   document.getElementById("imageClick2").innerHTML = "Congratulations, you found my javascript";}
+document.getElementById("secondImageClick").onclick = function() {onClickImage()};
+
+function onClickImage() {
+  document.getElementById("secondImageClick").innerHTML = "This is a differently written function! Javascript still kinda makes no sense to me"};
