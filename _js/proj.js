c0160378f4ee93bebad37dc85c17af2737beba07
VintageMocha <zendromida@gmail.com>
2019-05-10 09:34:24 -0700

    Update proj.js 5/10
    Updated files

--- _js/proj.js
+++ _js/proj.js
@@ -2,9 +2,20 @@
 
 var myArray = ["joker1.png", "illustrationexample.jpg", "mcreeGif.gif", "mercyGif.gif", "pharaGif.gif", "ZiShop.png"];
 
-var mySelector = function () {
-  Math.floor((Math.random() * 10) + 1);
+function randomNumber () {
+  var randNum = Math.floor(Math.random()*(10)+1);
+  return randNum;
 }
+
+function logNumber () {
+  var randNum = randomNumber();
+  if(randNum == 1, 2, 3, 4, 5, 6, 7, 8, 9, 10) {
+    console.log('You rolled a dice!')
+    console.log(randNum)
+  }
+}
+
+logNumber();
 
 //just so that there's some finished javascript - i cant' get the RNG to freakin' work..
 
@@ -17,5 +28,3 @@ document.getElementById("secondImageClick").onclick = function() {onClickImage()
 
 function onClickImage() {
   document.getElementById("secondImageClick").innerHTML = "also please buy my art even if these are filler images"};
-
-console.log(mySelector);
